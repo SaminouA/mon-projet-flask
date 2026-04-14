8da1d052481ef95e991d66a180ed1f49d812bcd3
Nadira <nadirasaminou@gmail.com>
2026-04-14 17:17:48 +0200

    fix: correction flake8 finale

--- src/app.py
+++ src/app.py
@@ -2,21 +2,26 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return jsonify({"message": "Bienvenue sur mon API", "status": "ok"})
+
 
 @app.route("/health")
 def health():
     return jsonify({"status": "healthy"})
 
+
 @app.route("/hello/<name>")
 def hello(name):
     return jsonify({"message": f"Bonjour {name} !"})
 
+
 @app.route("/add/<a>/<b>")
 def add(a, b):
     return jsonify({"result": int(a) + int(b)})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
